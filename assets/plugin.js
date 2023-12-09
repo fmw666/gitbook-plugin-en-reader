5d32582432830196205e47ffd94caa64f2a418b0
Maovo <34910514+fmw666@users.noreply.github.com>
2023-12-09 18:58:30 +0800

    pause the last audio when start a new audio

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let currentAudio = null;
+
   const wordElements = document.querySelectorAll('.plug_sound');
 
   wordElements.forEach(element => {
       element.addEventListener('click', () => {
+        // pause the currentAudio
+        if (currentAudio) {
+          currentAudio.pause();
+        }
+
         const audioWds = element.getAttribute('wd');
         const accent = element.hasAttribute('accent') ? element.getAttribute('accent') : '1';
         const audioSrc = `https://dict.youdao.com/dictvoice?audio=${audioWds}&type=${accent}`;
         const audio = new Audio(audioSrc);
+
+        // set currentAudio
+        currentAudio = audio;
+
         audio.play();
       });
   });
